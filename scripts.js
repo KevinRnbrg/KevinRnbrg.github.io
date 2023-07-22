859cc2f70c5f42b277eab8285a859631fe8b4bb8
Kevin Rosenberg <kevinrosenberg04@gmail.com>
2023-07-22 23:20:28 +0300

    Changes listed in wiki - 23.07.2023

--- scripts.js
+++ scripts.js
@@ -1,13 +1,14 @@
 gameContainer = document.getElementById("game-container");
 gameCanvas = document.getElementById("game-canvas");
 
-let spaceShip;
-let greenEnemy;
-let shipBullet;
-let enemyBullet;
-let randomSpeed;
+let playerSpaceship;
+let playerBullet;
 
-let enemyHitSound;
+let enemyAlien;
+let enemyBullet;
+
+const enemyBullets = [];
+let gameObjects = [];
 
 let winCount = 0;
 let loseCount = 0;
@@ -16,50 +17,34 @@ let score = 0;
 let gamePause;
 let gameStart;
 
+let lastShotTime = 0;
+const maxTimeBetweenShots = 2500;
+let lastFrameTime = Date.now();
+
 function startGame() {
     if (gameStart == true) {
         gameArea.stop();
         gameArea.clear();
     }
     gameArea.start();
+    gameObjects = [];
 
-    randomSpeed = Math.floor(Math.random() * 20) + 5; // speed is random number from 5...20
+    let randomDirection = Math.random() < 0.5 ? -1 : 1;
 
-    spaceShip = new component(110, 50, "sprites/spaceship03.png", 350, 650, 0, true, "image");
-    greenEnemy = new component(60, 60, "sprites/alien02.png", 350, 100, randomSpeed, true, "image");
-    shipBullet = new component(25, 25, "sprites/blueball.png", spaceShip.x, spaceShip.y, 0, true, "image");
-    enemyBullet = new component(25, 25, "sprites/redball.png", greenEnemy.x, greenEnemy.y, 0, true, "image");
+    playerSpaceship = new component(110, 50, "sprites/spaceship03.png", 350, 650, 0, true, "image");
+    enemyAlien = new component(60, 60, "sprites/alien02.png", 350, 100, ((5 + score*0.75) * randomDirection), true, "image");
+    playerBullet = new component(25, 25, "sprites/blueball.png", playerSpaceship.x, playerSpaceship.y, 0, true, "image");
+    enemyBullet = new component(25, 25, "sprites/redball.png", enemyAlien.x, enemyAlien.y, 0, true, "image");
+
+    gameObjects.push(playerSpaceship);
+    gameObjects.push(enemyAlien);
+    gameObjects.push(playerBullet);
+    gameObjects.push(enemyBullet);
     
     enemyHitSound = new sound("sounds/enemyHit2.m4a");
-    shipHitSound = new sound("sounds/spaceShipHit.m4a");
+    playerHitSound = new sound("sounds/spaceShipHit.m4a");
     enemyShootSound = new sound("sounds/enemyShoot2.m4a");
     shipShootSound = new sound("sounds/shipShoot.m4a");
-}
-
-document.addEventListener('keydown', function(event) { // space key disabled to stop scroll on page
-    if (event.code === 'Space' || event.code === 'ArrowUp' || event.code === 'ArrowDown' || event.code === 'ArrowLeft' || event.code === 'ArrowRight') {
-      event.preventDefault();
-    }
-});
-
-function stopGame() { // for testing purposes
-    console.log("gamePause: " + gamePause);
-    if (gamePause == false) {
-        gameArea.stop(); // paused state should display a message to know the game is paused
-    } else if (gamePause == true) {
-        gameArea.start();
-    }
-}
-
-function startGameBtn() {
-    clearGame()
-    startGame()
-}
-
-function clearGame() {
-    winCount = 0;
-    loseCount = 0;
-    score = 0;
 }
     
 let gameArea = {
@@ -73,13 +58,12 @@ let gameArea = {
         this.context = this.canvas.getContext("2d");
         //document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updateGameArea, 20);
-
         window.addEventListener('keydown', function (e) {
             gameArea.keys = (gameArea.keys || []);
             gameArea.keys[e.keyCode] = true;
         })
         window.addEventListener('keyup', function (e) {
-            gameArea.keys[e.keyCode] = false;
+                gameArea.keys[e.keyCode] = false;
         })
     },
     clear : function() {
@@ -89,18 +73,6 @@ let gameArea = {
         clearInterval(this.interval);
         gamePause = true;
     }
-}
-
-gameContainer.appendChild(gameCanvas);
-
-function playerWin() {
-    winCount += 1;
-    score += 1;
-} 
-
-function playerLose() {
-    loseCount += 1;
-    score -= 1;
 }
 
 function component(width, height, color, x, y, speed, alive, type) {
@@ -128,20 +100,24 @@ function component(width, height, color, x, y, speed, alive, type) {
                 ctx.fillStyle = color;
                 ctx.fillRect(this.x, this.y, this.width, this.height);
             }
-            
         }
     }
     this.kill = function () {
-        ctx = gameArea.context;
-        ctx.clearRect(this.x, this.y, this.width, this.height);
+        let index = gameObjects.indexOf(this);
+        if (index !== -1) {
+            gameObjects.splice(index, 1);
+        }
+        this.x = -100;
+        this.y = -100;
+
         this.alive = false;
     }
     this.updatePattern = function () { // enemy move pattern
         this.x += this.speedX;
         if (this.x >= 650) {
-            this.speedX = randomSpeed * -1;
+            this.speedX = (5 + score*0.75) * -1;
         } else if (this.x <= 50) {
-            this.speedX = randomSpeed;
+            this.speedX = (5 + score*0.75);
         }
     }
     this.newPos = function() {
@@ -156,7 +132,6 @@ function sound(src) {
     this.sound.setAttribute("preload", "auto");
     this.sound.setAttribute("controls", "none");
     this.sound.style.display = "none";
-    //document.body.appendChild(this.sound);
     this.play = function() {
         this.sound.play();
     }
@@ -165,44 +140,79 @@ function sound(src) {
     }
 }
 
-function checkCollision() { // check if spaceship bullet hit alien
-    if (greenEnemy.x < shipBullet.x + shipBullet.width && greenEnemy.x + greenEnemy.width > shipBullet.x && greenEnemy.y < shipBullet.y + shipBullet.height && greenEnemy.y + greenEnemy.height > shipBullet.y) {
-        if (greenEnemy.alive) {
-            enemyHitSound.play();
-        }
+document.addEventListener('keydown', function(event) { // space key disabled to stop scroll on page
+    if (event.code === 'Space' || event.code === 'ArrowUp' || event.code === 'ArrowDown' || event.code === 'ArrowLeft' || event.code === 'ArrowRight') {
+      event.preventDefault();
+    }
+});
+
+function stopGame() {
+    gameArea.context.font = " bold 30px 'Courier New', monospace";
+    gameArea.context.textAlign = "center";
+    gameArea.context.fillStyle = "Orange";
+    gameArea.context.fillText("Paused", gameArea.canvas.width/2, 330);
+    if (gamePause == false) {
+        gameArea.stop();
+    } else if (gamePause == true) {
+        gameArea.start();
+    }
+}
+
+function startGameBtn() {
+    clearGame()
+    startGame()
+}
+
+function clearGame() {
+    winCount = 0;
+    loseCount = 0;
+    score = 0;
+}
+
+gameContainer.appendChild(gameCanvas);
+
+function playerWin() {
+    winCount += 1;
+    score += 1;
+} 
+
+function playerLose() {
+    loseCount += 1;
+    score -= 1;
+}
+
+function checkCollision(subject, target) {
+    if (subject.x < target.x + target.width && 
+        subject.x + subject.width > target.x && 
+        subject.y < target.y + target.height && 
+        subject.y + subject.height > target.y) {
         return true;
     } else { return false; }
 }
 
-function checkShipCollision() { // check if alien bullet hit spaceship
-    if (spaceShip.x < enemyBullet.x + enemyBullet.width && spaceShip.x + spaceShip.width > enemyBullet.x && spaceShip.y < enemyBullet.y + enemyBullet.height && spaceShip.y + spaceShip.height > enemyBullet.y) {
-        if (spaceShip.alive) {
-            shipHitSound.play();
-        }
-        return true;
-    } else { return false; }
-}
-
-function updateShootPattern() {
+//the function should be remade for consistency
+function updateShootPattern() { // random number from 1-200
     random = Math.random();
-    randomNumber = random * 200; // random number from 1-200
+    randomNumber = random * 200;
     return randomNumber;
 }
 
 function restart() { // restart game
-    if (!spaceShip.alive && greenEnemy.alive) {
+    if (!playerSpaceship.alive && enemyAlien.alive) {
         playerLose();
-    } else if (spaceShip.alive && !greenEnemy.alive) {
+    } else if (playerSpaceship.alive && !enemyAlien.alive) {
         playerWin();
     }
     gameArea.stop();
     gameArea.clear();
+    gameObjects = [];
     startGame();
 }
 
-function playerRecord() { // show player record
+function displayPlayerRecord() { // show player record
     gameArea.context.font = "bold 30px 'Courier New', monospace";
-    gameArea.context.fillStyle = "red";
+    gameArea.context.textAlign = 'left';
+    gameArea.context.fillStyle = "cyan";
     if (score < 0) {
         score = 0;
     }
@@ -211,139 +221,170 @@ function playerRecord() { // show player record
     gameArea.context.fillText("Points: " + score, 30, 50);
 }
 
-function displayEndScreen() {    // 'You win' when enemy is dead, 'Your lose' when spaceship is dead, 'Draw' when both die
+function displayPlayerAndEnemyAttributes() {
+    gameArea.context.font = "bold 30px 'Courier New', monospace";
+    gameArea.context.textAlign = 'left';
+    gameArea.context.fillStyle = "cyan";
+    gameArea.context.fillText("Alien speed: " + (Math.abs(enemyAlien.speedX)), 30, 85);
+    //gameArea.context.fillText("Spaceship bullet speed: " + playerBullet.speedY, 30, 110);
+}
+
+function displayEndScreen() {    // 'You win', 'Your lose' and 'Draw' texts
+    centerX = gameArea.canvas.width/2;
+    centerY = gameArea.canvas.height/2;
+    const rectWidth = 400;
+    const rectHeight = 150;
+    rectX = centerX - rectWidth / 2; 
+    rectY = centerY - rectHeight / 2;
+
     gameArea.context.font = " bold 30px 'Courier New', monospace";
-    if (!spaceShip.alive && greenEnemy.alive) {
-        gameArea.context.fillStyle = "Red";
-        gameArea.context.fillText("You lose", 350, 375);
-        gameArea.context.fillText("Press R to restart", 275, 425);
+    gameArea.context.textAlign = 'center';
+    if (!enemyAlien.alive || !playerSpaceship.alive) {
+        gameArea.context.fillStyle = "Gray";
+        gameArea.context.fillRect(rectX, rectY, rectWidth, rectHeight)
+        gameArea.context.strokeStyle = "Cyan";
+        gameArea.context.lineWidth = 10;
+        gameArea.context.strokeRect(rectX, rectY, rectWidth, rectHeight);
+        gameArea.context.fillStyle = "Cyan";
+        gameArea.context.fillText("Press R to restart", centerX, 410);
     }
-    if (!greenEnemy.alive && spaceShip.alive) {
-        gameArea.context.fillStyle = "Green";
-        gameArea.context.fillText("You win", 350, 375);
-        gameArea.context.fillText("Press R to restart", 250, 425);
+    if (!playerSpaceship.alive && enemyAlien.alive) {
+        gameArea.context.fillStyle = "#FF3131";
+        gameArea.context.fillText("You lose", centerX, 360);
     }
-    if (!greenEnemy.alive && !spaceShip.alive) {
+    if (!enemyAlien.alive && playerSpaceship.alive) {
+        gameArea.context.fillStyle = "#0FFF50";
+        gameArea.context.fillText("You win", centerX, 360);
+    }
+    if (!enemyAlien.alive && !playerSpaceship.alive) {
         gameArea.context.fillStyle = "Orange";
-        gameArea.context.fillText("Draw", 350, 375);
-        gameArea.context.fillText("Press R to restart", 300, 425);
+        gameArea.context.fillText("Draw", centerX, 360);
     }
 }
 
-function stopEdgeMovement() { // check if object is near edge and stop it
-    /* can be remade by doing stopEdgeMovement(spaceShip) and then catch with object and object.speed = 0; */ 
-
-    if (spaceShip.x > 650) {
+function stopObjectAtEdge(object) {
+    if (object.x > 650) {
         if (gameArea.keys[39] || gameArea.keys[68]){
-            spaceShip.speedX = 0;
+            object.speedX = 0;
         }
     }
-    if (spaceShip.x < 40) {
+    if (object.x < 40) {
         if (gameArea.keys[37] || gameArea.keys[65]){
-            spaceShip.speedX = 0;
+            object.speedX = 0;
         }
     }
-    if (spaceShip.y > 650) {
+    if (object.y > 650) {
         if (gameArea.keys[40] || gameArea.keys[83]){
-            spaceShip.speedY = 0;
+            object.speedY = 0;
         }
     }
-    if (spaceShip.y < 500) {
+    if (object.y < 500) {
         if (gameArea.keys[38] || gameArea.keys[87]){
-            spaceShip.speedY = 0;
+            object.speedY = 0;
         }
-    }
-
-    // stop enemy if it is near border
-    if (greenEnemy.x > 800) {
-        greenEnemy.speedX = 0;
-    }
-    if (greenEnemy.x < 0) {
-        greenEnemy.speedX = 0;
     }
 }
 
-function movementControls() {
-    // spaceship stops after release of movement button
-    spaceShip.speedX = 0;
-    spaceShip.speedY = 0;
+function playerMovementControls() {
+    // playerSpaceship stops after release of movement button
+    playerSpaceship.speedX = 0;
+    playerSpaceship.speedY = 0;
 
     // arrowkeys movement
-    if (gameArea.keys && gameArea.keys[37]) {spaceShip.speedX = -10; }
-    if (gameArea.keys && gameArea.keys[39]) {spaceShip.speedX = 10; }
-    if (gameArea.keys && gameArea.keys[38]) {spaceShip.speedY = -10; }
-    if (gameArea.keys && gameArea.keys[40]) {spaceShip.speedY = 10; }
+    if (gameArea.keys && gameArea.keys[37]) {playerSpaceship.speedX = -10; }
+    if (gameArea.keys && gameArea.keys[39]) {playerSpaceship.speedX = 10; }
+    if (gameArea.keys && gameArea.keys[38]) {playerSpaceship.speedY = -10; }
+    if (gameArea.keys && gameArea.keys[40]) {playerSpaceship.speedY = 10; }
 
     // WASD movement
-    if (gameArea.keys && gameArea.keys[65]) {spaceShip.speedX = -10; }
-    if (gameArea.keys && gameArea.keys[68]) {spaceShip.speedX = 10; }
-    if (gameArea.keys && gameArea.keys[87]) {spaceShip.speedY = -10; }
-    if (gameArea.keys && gameArea.keys[83]) {spaceShip.speedY = 10; }
+    if (gameArea.keys && gameArea.keys[65]) {playerSpaceship.speedX = -10; }
+    if (gameArea.keys && gameArea.keys[68]) {playerSpaceship.speedX = 10; }
+    if (gameArea.keys && gameArea.keys[87]) {playerSpaceship.speedY = -10; }
+    if (gameArea.keys && gameArea.keys[83]) {playerSpaceship.speedY = 10; }
 }
 
-function bulletMechanics() {
-    // ship bullet move when space pressed, otherwise stay with ship
+function playerBulletMechanics() {
     if (gameArea.keys && gameArea.keys[32]) {
-        if (shipBullet.speedY == 0) {
-            shipShootSound.play(); // play enemy collision sound even when enemy dead
+        if (playerBullet.speedY == 0) { // if bullet is with ship and space pressed
+            shipShootSound.play();
         }
-        if (score >= 10) { // if score is atleast 10 then bullet is faster by 5
-            shipBullet.speedY = -20;
+        if (score >= 10) { // bullet faster when score >= 10
+            playerBullet.speedY = -20;
         } else {
-            shipBullet.speedY = -15; 
+            playerBullet.speedY = -15; 
         }
-    } else if (shipBullet.y <= 0 || shipBullet.speedY == 0) {
-            shipBullet.speedY = 0;
-            shipBullet.x = spaceShip.x + 40;
-            shipBullet.y = spaceShip.y - 30;
+    } else if (playerBullet.y <= 0 || playerBullet.speedY == 0) {
+            playerBullet.speedY = 0;
+            playerBullet.x = playerSpaceship.x + 40;
+            playerBullet.y = playerSpaceship.y - 30;
     }  
+}
 
+function enemyBulletMechanics() {
     // enemy bullet stay with enemy, shoot once in a while
-    if (updateShootPattern() > 190)  {
-        if (enemyBullet.speedY == 0) {
+    if (enemyAlien.alive) {
+        shootTime = maxTimeBetweenShots - (50*score)
+        if (lastShotTime > shootTime) {
+            lastShotTime = 0;
+            enemyBullet.speedY = 18;
             enemyShootSound.play();
         }
-        enemyBullet.speedY = 18;
-    } else if (enemyBullet.y >= 750 || enemyBullet.speedY == 0) {
-        enemyBullet.speedY = 0;
-        enemyBullet.x = greenEnemy.x + 20;
-        enemyBullet.y = greenEnemy.y + 65;
-    }
+        else if (enemyBullet.y >= 750 || enemyBullet.speedY == 0) {
+            enemyBullet.speedY = 0;
+            enemyBullet.x = enemyAlien.x + 20;
+            enemyBullet.y = enemyAlien.y + 65;
+        }
+    } else { return; }
 }
 
+function calculateLastEnemyShot() {
+    const now = Date.now();
+    const deltaTime = now - lastFrameTime;
+    lastFrameTime = now;
+    lastShotTime += deltaTime;
+}
 
 function updateGameArea() {
     gameArea.clear();
 
-    playerRecord();
-    displayEndScreen();
-    
-    movementControls();
-    bulletMechanics();
-    stopEdgeMovement();
+    displayPlayerAndEnemyAttributes();
+    displayPlayerRecord();
+    playerMovementControls();
+    enemyBulletMechanics();
+    playerBulletMechanics();
+    calculateLastEnemyShot();
+    stopObjectAtEdge(playerSpaceship);
 
     // restart game with 'R'
-    if (gameArea.keys && gameArea.keys[82]) {
-        restart(); 
+    if (gameArea.keys && gameArea.keys[82]) { // can hold, should be on button up or smth like that
+        restart();
     }
     
-    spaceShip.newPos();
-    if (checkShipCollision() || spaceShip.alive == false) { // if ship collided with enemy bullet then die
-        spaceShip.kill();
-        shipBullet.kill();
-    } else { 
-        spaceShip.update();
-        shipBullet.newPos();
-        shipBullet.update();
+    for (let i = 0; i < gameObjects.length; i++) {
+        let gameObject = gameObjects[i];
+
+        if (gameObject == enemyAlien) {
+            gameObject.updatePattern();
+        } else {
+            gameObject.newPos();
+        }
+        gameObject.update();
     }
 
-    greenEnemy.updatePattern();
-    if (checkCollision() || greenEnemy.alive == false) { // if enemy has collided with ship bullet then die
-        greenEnemy.kill();
+    displayEndScreen();
+
+    if (checkCollision(playerSpaceship, enemyBullet)) {
+        if (playerSpaceship.alive) {
+            playerHitSound.play();
+        }
+        playerSpaceship.kill();
+        playerBullet.kill();
+    }
+    if (checkCollision(enemyAlien, playerBullet)) {
+        if (enemyAlien.alive) {
+            enemyHitSound.play();
+        }
+        enemyAlien.kill();
         enemyBullet.kill();
-    } else {
-        greenEnemy.update();
-        enemyBullet.newPos();
-        enemyBullet.update();
     }
 }
